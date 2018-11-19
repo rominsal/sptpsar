01b93fc476f8a72e508ac21e71048cb3cce9757e
Román Mínguez <roman.minguez@uclm.es>
2018-11-19 11:06:20 +0100

    Delete bspline.R

--- R/bspline.R
+++
@@ -1,7 +0,0 @@
-bspline <- function(x, xl, xr, ndx, bdeg){
-	dx <- (xr - xl)/ndx
-	knots <- seq(xl - bdeg*dx, xr + bdeg*dx, by=dx)
-	B <- splines::spline.des(knots, x, bdeg+1, 0*x)$design
-	res <- list(B = B, knots = knots)
-	res
-}
